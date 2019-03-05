57ccc48f5a15078cf3b9155072f1dc06aa915954
arkadyt <contact.me@arkadyt.dev>
2019-03-05 13:11:58 -0800

    Update file names

---
+++ awms-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package awms-plugin
+ * @version 0.0.0
+ */
+/*
+Plugin Name: AWeber Multi-Subscription
+Description: TODO add description
+Author: Andrew Hendrix
+Version: 0.0.0
+Author URI: http://arkadyt.com
+*/
+
