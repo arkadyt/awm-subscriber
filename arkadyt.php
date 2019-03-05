628516ec42eb0c2bc35c2b64b7db2cbc1cfe6ae6
arkadyt <contact.me@arkadyt.dev>
2019-03-05 13:06:11 -0800

    Initial commit

---
+++ arkadyt.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package arkadyt-aweber-multisubscription
+ * @version 0.0.0
+ */
+/*
+Plugin Name: Arkadyt Auto Subscriber
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: Missing description!
+Author: Arkady Titenko
+Version: 0.0.0
+Author URI: http://arkadyt.com
+*/
+
