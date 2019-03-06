c0b00c58de9b8a2d8ad5501d7ebbaf3eae65e435
arkadyt <contact.me@arkadyt.dev>
2019-03-06 13:41:20 -0800

    Fix Activator call

--- inc/base/Activator.php
+++ inc/base/Activator.php
@@ -6,7 +6,7 @@
 namespace inc\base;
 
 final class Activator {
-  public function register() {
+  public function execute() {
     // setup options with dummy data
     // - API key
     // - Redirect page
