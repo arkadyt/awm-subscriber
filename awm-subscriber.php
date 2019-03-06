19802a4af0a9d0deac6d7061fd07e10880b39202
arkadyt <contact.me@arkadyt.dev>
2019-03-05 16:22:20 -0800

    Add sidenav button

--- awm-subscriber.php
+++ awm-subscriber.php
@@ -7,14 +7,12 @@
 /**
  * Plugin Name: AWeber Multi Subscriber
  * Plugin URI:  https://example.com/plugins/the-basics/
- * Description: TODO
+ * Description: Plugin subscribes a user to multiple AWeber lists once he subscribes to a single list of your choice and confirms the subscription. Learn more on the Settings page.
  * Version:     0.0.0
  * Author:      Andrew Hendrix
  * Author URI:  http://arkadyt.com
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: wporg
- * Domain Path: /languages
  */
 
 /* X Works only on /thank-you/ page, which is configured.
@@ -27,20 +25,51 @@
  *
  */
 
-class AWM_Subscriber {
-  function activate() {
+if (!defined('ABSPATH')) {
+  echo 'WARNING: Attempt to access plugin code outside of Wordpress environment. Access denied.';
+  exit;
+}
 
+class AWMSubscriber {
+  function activate() {
+    // setup options with dummy values:
+    // - aweber_key
+    // - thank_you_page_url
+    // Don't forget to prefix var names!
+
+    add_action('admin_menu', array($this, 'add_admin_section'));
+  }
+
+  function add_admin_section() {
+    add_menu_page(
+      'AWeber Multi Subscriber',
+      'AWM Subscriber',
+      'manage_options',
+      'awm_subscriber',
+      array($this, 'admin_index'),
+      'dashicons-admin-tools',
+      100
+    );
+  }
+
+  public function admin_index() {
+    // req template
   }
 
   function deactivate() {
-
+    // just don't get triggered any more
+    // don't have to do anything for this
   }
 
-  function uninstall() {
-
+  static function uninstall() {
+    // delete all options from the database
   }
 }
 
-register_activation_hook(__FILE__, 'awm_subscriber_activate');
-register_deactivation_hook(__FILE__, 'awm_subscriber_deactivate');
-register_uninstall_hook(__FILE__, 'awm_subscriber_uninstall');
+if (class_exists('AWMSubscriber')) {
+  $awmSubscriber = new AWMSubscriber('2PAC IS BAK');
+}
+
+register_activation_hook(__FILE__, array($awmSubscriber, 'awm_subscriber_activate'));
+register_deactivation_hook(__FILE__, array($awmSubscriber, 'awm_subscriber_deactivate'));
+register_uninstall_hook(__FILE__, array($awmSubscriber, 'awm_subscriber_uninstall'));
