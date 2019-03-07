5c7524bcecc9cbe1a7a377b11a7bf88ad1c18e93
arkadyt <contact.me@arkadyt.dev>
2019-03-06 17:05:30 -0800

    Extract confirm_page_url from AWeber request url

--- inc/base/Subscriber.php
+++ inc/base/Subscriber.php
@@ -18,21 +18,22 @@ final class Subscriber extends BaseController {
    * Allows WP_Query to recognize custom variables from the request url.
    */
   public function add_query_vars_filter($vars) {
-    $vars[] = "kaka";
+    $vars[] = "email";
+    $vars[] = "add_url";
     return $vars;
   }
 
   /**
    * Extracts aweber list id's from the AWeber request url.
    */
-  public function get_aweber_lists($full_url) {
+  public function get_aweber_lists($current_page_fullurl) {
     // example AWeber request url (all joined):
     // ?email=wp-testing%40arkadyt.com&from=wp-testing%40arkadyt.com&meta_adtracking=my%20web%20form&meta_message=1001
     // &name=Thug&unit=awlist5279237&add_url=http%3A%2F%2Fwp-testing.arkadyt.com%2Fthank-you%2F&add_notes=255.255.255.255
     // &custom%20awlist5279237=yes&custom%20awlist5000207=yes&custom%20awlist01290129=yes
 
     // splits url into chunks
-    $parsed_url = parse_url($full_url);
+    $parsed_url = parse_url($current_page_fullurl);
     // decode query string
     parse_str($parsed_url['query'], $query_str);
 
@@ -58,14 +59,20 @@ final class Subscriber extends BaseController {
     global $wp;
 
     $current_page_slug = $wp->query_vars['pagename'];
-    $confirm_page_slug = 'thank-you';
+    $current_page_url = home_url() . $current_page_slug;
 
-    $full_url = home_url(add_query_arg(array($_GET), $wp->request));
-    $aweber_lists = $this->get_aweber_lists($full_url);
+    $confirm_page_url = $wp->query_vars['add_url'];
+    $confirm_page_slug = parse_url($confirm_page_url)['path'];
+
+    // full url contains query parms string
+    $current_page_fullurl = home_url(add_query_arg(array($_GET), $wp->request));
+    $aweber_lists = $this->get_aweber_lists($current_page_fullurl);
 
     if ($current_page_slug === $confirm_page_slug) {
       echo 'Subscribing user...<br/>';
       printf('<pre>%s</pre>', var_export($aweber_lists, true));
+      // send post requests
+      // on success do redirect
     } else {
       echo 'Doing nothing on this page.';
     }
