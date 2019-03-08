4bc7cda5c128bf57230c2691dae6fd47b5f9fad8
arkadyt <contact.me@arkadyt.dev>
2019-03-07 16:21:41 -0800

    Add important comments to Subscriber.php

--- inc/base/Subscriber.php
+++ inc/base/Subscriber.php
@@ -2,10 +2,14 @@
 /**
  * @package awm-subscriber
  */
-// example AWeber request url (all joined):
-// ?email=wp-testing%40arkadyt.com&from=wp-testing%40arkadyt.com&meta_adtracking=my%20web%20form&meta_message=1001
-// &name=Thug&unit=awlist5279237&add_url=http%3A%2F%2Fwp-testing.arkadyt.com%2Fthank-you%2F&add_notes=255.255.255.255
-// &custom%20awlist5279237=yes&custom%20awlist5000207=yes&custom%20awlist01290129=yes
+
+/**
+ * example AWeber request url (all joined):
+ *
+ * ?email=wp-testing%40arkadyt.com&from=wp-testing%40arkadyt.com&meta_adtracking=my%20web%20form&meta_message=1001
+ * &name=Thug&unit=awlist5279237&add_url=http%3A%2F%2Fwp-testing.arkadyt.com%2Fthank-you%2F&add_notes=255.255.255.255
+ * &custom%20awlist5279237=yes&custom%20awlist5000207=yes&custom%20awlist01290129=yes
+ */
 
 namespace inc\base;
 
@@ -13,6 +17,10 @@ namespace inc\base;
  * Core service of this plugin.
  */
 final class Subscriber extends BaseController {
+  /**
+   * Installs various hooks.
+   * Do not change the name of the method.
+   */
   public function register() {
     add_filter('query_vars', array($this, 'add_query_vars_filter'));
     add_action('init', array($this, 'intercept_get_request'));
@@ -22,6 +30,9 @@ final class Subscriber extends BaseController {
   /**
    * Unsets query variables forbidden by Wordpress.
    * Like 'name', AWeber sends it in the GET request.
+   *
+   * Presence of those in the query string causes 
+   * Wordpress to respond with 404 page.
    */
   public function intercept_get_request() {
     if (isset($_GET['name'])) {
@@ -31,7 +42,7 @@ final class Subscriber extends BaseController {
   }
 
   /**
-   * Allows WP_Query to recognize custom variables from the request url.
+   * Teaches WP_Query to recognize custom variables from the request url.
    */
   public function add_query_vars_filter($vars) {
     $vars[] = "email";
@@ -41,6 +52,19 @@ final class Subscriber extends BaseController {
 
   /**
    * Extracts aweber list id's from the AWeber request url.
+   * Since those are dynamic, I can't use query vars filter hook.
+   *
+   * The dynamic nature comes from the way users of this plugin
+   * will setup their custom checkboxes on the form.
+   * 
+   * AWeber sends custom_field_names=yes per every checked checkbox,
+   * and at the moment of writing this is the only way of receiving
+   * the information from AWeber.
+   *
+   * Giving checkbox fields names corresponding to their respective
+   * AWeber lists allows me to use this information here.
+   *
+   * Though with one drawback, I have to use manual parser.
    */
   public function extract_awlists_from_url($current_page_fullurl) {
     // splits url into chunks: protocol, user, password, host, port, path, query etc.
@@ -71,7 +95,7 @@ final class Subscriber extends BaseController {
     $confirm_page_url = $wp->query_vars['add_url'];
     $confirm_page_slug = str_replace('/', '', parse_url($confirm_page_url)['path']);
 
-    // full url contains query parms string
+    // full url contains (raw) query parms string
     $current_page_fullurl = home_url(add_query_arg(array($_GET), $wp->request));
     $aweber_lists = $this->extract_awlists_from_url($current_page_fullurl);
 
