21f351c246a25c20aaadb27d0560e2a528fbb471
arkadyt <contact.me@arkadyt.dev>
2019-03-09 10:32:14 -0800

    Register settings (wp req.) for options page

--- inc/pages/Settings.php
+++ inc/pages/Settings.php
@@ -14,25 +14,36 @@ final class Settings extends BaseController {
    */
   public function register() {
     add_action('admin_menu', array($this, 'add_settings_page'));
+    add_action('admin_init', array($this, 'register_settings'));
   }
 
   /**
-   * Adds new 'Admin' page
+   * Adds new 'Settings' page
    */
   public function add_settings_page() {
     add_options_page(
       'AWM Subscriber',
       'AWM Subscriber',
       'manage_options',
-      'awm_subscriber_settings',
+      $this->groupname_plugin_settings,
       array($this, 'get_settings_page_template')
     );
   }
 
   /**
-   * Returns path to Admin page template.
+   * Returns path to Settings page template.
    */
   public function get_settings_page_template() {
     require_once $this->plugin_root . 'templates/settings.php';
+  }
+
+  /**
+   * Registers settings that form on the template will use.
+   */
+  public function register_settings() {
+    register_setting(
+      $this->groupname_plugin_settings,
+      $this->optname_response_code
+    );
   }
 }
