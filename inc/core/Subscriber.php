d0c461279d78212b861e2956dac6d9d40c6f1929
arkadyt <contact.me@arkadyt.dev>
2019-03-09 13:30:35 -0800

    Rewrite Subscriber.php

--- inc/core/Subscriber.php
+++ inc/core/Subscriber.php
@@ -13,15 +13,43 @@
 
 namespace inc\core;
 
+use inc\api\AWeberIntegration;
 use inc\base\BaseController;
-use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
 /**
  * Core service of this plugin.
  */
 final class Subscriber extends BaseController {
+  private const OPTNAME_AWEBER_CUSTOMER_ID = 'awm_subscriber_customer_id';
+
+  public $authorize_url;
+
+  private $aweber_client;
+
+  /**
+   * Spawns AWeber client and prepares authorization url.
+   */
+  public function __construct() {
+    parent::__construct();
+    $this->aweber_client = new AWeberIntegration($this->app_id);
+    $this->authorize_url = $this->aweber_client->get_authorize_url();
+  }
+
+  /**
+   * Initializes the Subscriber service.
+   * Can't merge it with register() function since we depend on
+   * the response code supplied by user upon plugin authorization.
+   */
+  public function initialize($response_code) {
+    $this->aweber_client->initialize($response_code);
+
+    // AWeber does not provide a way to get the current user.
+    // That means that every user of this plugin would have to use
+    // a separate developer app.
+    $id = $this->aweber_client->get('accounts')['entries'][0]['id'];
+    update_option(self::OPTNAME_AWEBER_CUSTOMER_ID, $id);
+  } 
+
   /**
    * Installs various hooks.
    * Do not change the name of the method.
@@ -29,7 +57,7 @@ final class Subscriber extends BaseController {
   public function register() {
     add_filter('query_vars', array($this, 'add_query_vars_filter'));
     add_action('init', array($this, 'intercept_get_request'));
-    add_action('wp', array($this, 'subscribe'));
+    add_action('wp', array($this, 'attempt_to_subscribe'));
   }
 
   /**
@@ -56,20 +84,8 @@ final class Subscriber extends BaseController {
   }
 
   /**
-   * Extracts aweber list id's from the AWeber request url.
-   * Since those are dynamic, I can't use query vars filter hook.
-   *
-   * The dynamic nature comes from the way users of this plugin
-   * will setup their custom checkboxes on the form.
-   * 
-   * AWeber sends custom_field_names=yes per every checked checkbox,
-   * and at the moment of writing this is the only way of receiving
-   * the information from AWeber.
-   *
-   * Giving checkbox fields names corresponding to their respective
-   * AWeber lists allows me to use this information here.
-   *
-   * Though with one drawback, I have to use manual parser.
+   * Though WP_Query does not work with dynamic parms.
+   * Extracts dynamic aweber list id's from the AWeber request url.
    */
   public function extract_awlists_from_url($current_page_fullurl) {
     // splits url into chunks: protocol, user, password, host, port, path, query etc.
@@ -80,18 +96,16 @@ final class Subscriber extends BaseController {
     $aweber_lists = array();
     foreach ($query_str as $key => $value) {
       if ($value === 'yes' && strpos($key, 'awlist') !== false) {
-        $aweber_lists[] = str_replace('custom_', '', $key);
+        $aweber_lists[] = str_replace('custom_awlist', '', $key);
       }
     }
     return $aweber_lists;
   }
 
   /**
-   * Will try to subscribe user to multiple AWeber lists of his choice
-   * once he visits the confirmation page.
-   * AWeber supplies chosen list id-s in the GET url.
+   * Calls the subscribe function if user clicked on the confirmation link from email.
    */
-  public function subscribe() {
+  public function attempt_to_subscribe() {
     global $wp;
 
     $current_page_slug = $wp->query_vars['pagename'];
@@ -100,68 +114,34 @@ final class Subscriber extends BaseController {
     $confirm_page_url = $wp->query_vars['add_url'];
     $confirm_page_slug = str_replace('/', '', parse_url($confirm_page_url)['path']);
 
-    // full url contains (raw) query parms string
-    $current_page_fullurl = home_url(add_query_arg(array($_GET), $wp->request));
-    $aweber_lists = $this->extract_awlists_from_url($current_page_fullurl);
+    // Confirmation page has been visited manually.
+    // It does not contain query parms.
+    if (!$wp->query_vars['email']) {
+      return;
+    }
 
     if ($current_page_slug === $confirm_page_slug) {
-      echo 'Subscribing user...<br/>';
-      // printf('<pre>%s</pre>', var_export($aweber_lists, true));
-      // send post requests
-      $this->test_posting();
-    } else {
-      echo 'Doing nothing on this page. Checks done: ' . $current_page_slug . ' === ' . $confirm_page_slug;
+      // full url contains (raw) query parms string
+      $current_page_fullurl = home_url(add_query_arg(array($_GET), $wp->request));
+      $aweber_lists = $this->extract_awlists_from_url($current_page_fullurl);
+      $this->subscribe($wp->query_vars['email'], $aweber_lists);
     }
   }
 
-  public function test_posting() {
-    $stack = HandlerStack::create();
-    $client = new Client([
-      'base_uri' => 'https://api.aweber.com/1.0/',
-      'handler' => $stack,
-      'auth' => 'oauth'
-    ]);
-    
-    $app_id = 'dadb3b05';
-    $access_token_url = 'https://auth.aweber.com/1.0/oauth/access_token';
-    $authorize_url = "https://auth.aweber.com/1.0/oauth/authorize_app/$app_id";
-    $api_url = 'https://api.aweber.com/1.0/accounts';
-
-    // if authenticating through app_id remember that you get only one
-    // attempt per $authorization_code. debugging it is painful.
-    $authorization_code = 'AzPHI215Es40C0Eagn2yrZEI|uL4RDPanw8q8i87uPdnMG4rN1i3zF2L68PHdamgL|AqXAQPlVTGg6Y7QLxO1AXf4X|wFOJQlWOXoGXig7lIwuLBURol0hDTj0QwoSc1SV8|osvry8|';
-    $authorization_code_exploded = explode('|', $authorization_code);
-    $auth = new Oauth1([
-      'consumer_key'    => $authorization_code_exploded[0],
-      'consumer_secret' => $authorization_code_exploded[1],
-      'token'           => $authorization_code_exploded[2],
-      'token_secret'    => $authorization_code_exploded[3],
-      'verifier'        => $authorization_code_exploded[4]
-    ]);
-    $stack->push($auth);
-    // $res = $client->post($access_token_url);
-
-    // $res_body = $res->getBody();
-    // echo "RES->BODY: $res_body";
-    // Save the decoded response ($keys) in database along with 
-    // consumer_secret and consumer_key generated in the previous step.
-    $res_body = 'oauth_token_secret=9eAI3hUW5ByMibtJFm6Lt04YLB7dshw175hLAj4M&oauth_token=AgsqKpz3YaErM5z5s40qPrcV';
-    $keys = array();
-    parse_str($res_body, $keys);
-    $stack->remove($auth);
-
-    $auth = new Oauth1(array(
-      'consumer_key'    => $authorization_code_exploded[0],
-      'consumer_secret' => $authorization_code_exploded[1],
-      'token'           => $keys['oauth_token'],
-      'token_secret'    => $keys['oauth_token_secret']
-    ));
-    $stack->push($auth);
-    $res = $client->get('accounts');
-    $res_body = $res->getBody();
-    echo "RES? $res_body";
-    
-    return $auth;
+  /**
+   * Subscribes user to multiple AWeber lists of his choice.
+   */
+  public function subscribe($subscriberEmail, $aweber_lists) {
+    $aweber_customer_id = get_option(self::OPTNAME_AWEBER_CUSTOMER_ID);
+    foreach ($aweber_lists as $listId) {
+      $payload = array(
+        'email' => $subscriberEmail
+      );
+      $res = $this->aweber_client->post(
+        "accounts/$aweber_customer_id/list/$listId/subscribers",
+        $payload
+      );
+    }
+    return $res;
   }
 }
-
