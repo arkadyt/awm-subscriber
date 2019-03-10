b47c6a64d04d6e4e7c78fe4485b2f4846a5f81b5
arkadyt <contact.me@arkadyt.dev>
2019-03-09 18:09:52 -0800

    Update plugin readme

--- templates/settings.php
+++ templates/settings.php
@@ -1,15 +1,15 @@
 <?php 
-  use inc\Init;
-  use inc\core\Subscriber;
+use inc\Init;
+use inc\core\Subscriber;
 
-  $awm_sub_sett_optname_rcode = 'awm_subscriber_response_code';
-  $awm_sub_sett_subs_instance = Init::get_instance(Subscriber::class);
+$awm_sub_sett_optname_rcode = 'awm_subscriber_response_code';
+$awm_sub_sett_subs_instance = Init::get_instance(Subscriber::class);
 
-  $awm_sub_sett_authorize_url = $awm_sub_sett_subs_instance->authorize_url;
-  $awm_sub_sett_response_code = get_option($awm_sub_sett_optname_rcode);
+$awm_sub_sett_authorize_url = $awm_sub_sett_subs_instance->authorize_url;
+$awm_sub_sett_response_code = get_option($awm_sub_sett_optname_rcode);
 
-  $awm_sub_sett_is_first_time = !$awm_sub_sett_response_code;
-  $awm_sub_sett_is_initialized = $awm_sub_sett_response_code === 'initialized';
+$awm_sub_sett_is_first_time = !$awm_sub_sett_response_code;
+$awm_sub_sett_is_initialized = $awm_sub_sett_response_code === 'initialized';
 ?>
 
 <div class="wrap">
@@ -62,46 +62,53 @@
       <input type="hidden" name="<?php echo $awm_sub_sett_optname_rcode ?>" value="" />
       <?php submit_button('Remove authorization'); ?>
     </form>
-
-    <h1>How this plugin works:</h1>
-    <h2>1. Visitor subscribes to your main AWeber list.</h2>
-    <p>
-      Bob comes to your website, decides he'd like to receive news from you in his email inbox.<br/>
-      He fills out the form and discovers that he can subscribe to additional lists as well.<br/>
-      He makes a selection and proceeds by clicking on <b>'Submit'</b>.
-    </p>
-    <h2>2. Visitor receives the confirmation email.</h2>
-    <p>
-      Bob receives confirmation email in his inbox, opens it up and clicks on <b>'Confirm'</b> button.<br/>
-      He is immediately taken to your website <b>where this plugin subscribes him to additional lists</b> of his choice.
-    </p>
-    <h2>3. Visitor receives additional confirmation emails.</h2>
-    <p>
-      Bob receives additional confirmation emails from every list he decided to subscribe to <b>additionally</b>.
-    </p></br>
-
-    <h1>Here's what's required from you:</h1>
-    <p>
-      Good news! You still manage everything related to email campaigns at AWeber website.<br/>
-      This plugin does not require any additional configuration on the Wordpress side.<br/><br/>
-
-      There are only two rules you have to follow at AWeber in order to enable automatic subscriptions:</br>
-    </p>
-    <h2>1. Make sure you use proper 'Field Names' for your custom fields on your AWeber form.</h2>
-    <p>
-      When you create a custom field for your AWeber form, it first asks you which '<b>Field Name</b>' you want to use,<br/>
-      Then on the next slide for 'Field Label', 'Field Type' and 'Tags'.<br/><br/>
-
-      Make sure that for every new field you create, you set the '<b>Field Name</b>' value to the '<b>ID</b>'<br/>
-      of the corresponding AWeber list (like <b>awlist5279237</b>).<br/>
-      Automatic subscriptions are not possible without an association system like this.
-    </p>
-    <h2>2. Make sure your main AWeber list redirects people to your website upon subscription confirmation.</h2>
-    <p>
-      You can set this setting in <b>List Options > List Settings > Confirmation Message > Confirmation Success Page</b>.<br/>
-      Redirection to your website is important because <b>that's where this plugin does it's job</b>, on your website.</br>
-      I recommend setting up a separate page for that. It doesn't have to be nothing fancy, but just being a <b>separate</b> page.
-    </p>
-
   <?php endif; ?>
+
+  <h1>How this plugin works:</h1>
+  <h2>1. Visitor subscribes to your main AWeber list.</h2>
+  <p>
+    Bob comes to your website, decides he'd like to receive news from you in his email inbox.<br/>
+    He fills out the form and discovers that he can subscribe to additional lists as well.<br/>
+    He makes a selection and proceeds by clicking on <b>'Submit'</b>.
+  </p>
+  <h2>2. Visitor receives the confirmation email.</h2>
+  <p>
+    Bob receives confirmation email in his inbox, opens it up and clicks on <b>'Confirm'</b> button.<br/>
+    He is immediately taken to your website <b>where this plugin subscribes him to additional lists</b> of his choice.
+  </p>
+  <h2>3. Visitor receives additional confirmation emails.</h2>
+  <p>
+    Bob receives additional confirmation emails from every list he decided to subscribe to <b>additionally.</b></br></br>
+    Please keep in mind: AWeber does not allow subscribing users to mailing lists without their permission.</br>
+    As they say, "That's done to prevent malicious subscriptions and false spam allegations".</br></br>
+    In the end, though, you don't have to create the whole myriad of forms and user doesn't have to fill them all out!</br>
+    User would just need to spend 10-30 seconds on confirming the additional subscriptions in his email inbox!
+  </p></br>
+
+  <h1>Here's what's required from you:</h1>
+  <p>
+    Good news! You still manage everything related to email campaigns at AWeber website.<br/>
+    This plugin does not require any additional configuration on the Wordpress side.<br/><br/>
+
+    There are only two rules you have to follow at AWeber in order to enable automatic subscriptions:</br>
+  </p>
+  <h2>1. Make sure you use proper 'Field Names' for your custom fields on your AWeber form.</h2>
+  <p>
+    When you create a custom field for your AWeber form (<b>List Options > Custom Fields</b>),<br/>
+    it first asks you which '<b>Field Name</b>' you want to use, then on the next slide,<br/>
+    it asks for 'Field Label' and some other options that are not relevant for the plugin.<br/><br/>
+
+    Make sure that for every new field you create, you set the '<b>Field Name</b>' value to<br/>
+    the '<b>ID</b>' of the corresponding AWeber list (like <b>awlist5279237</b>).<br/><br/>
+
+    Automatic subscriptions are not possible without an association system like this.<br/>
+    You can get a list ID at <b>List Options > List Settings > (look under the list name)</b> 
+  </p>
+  <h2>2. Make sure your main AWeber list redirects people to your website upon subscription confirmation.</h2>
+  <p>
+    By saying main list, I mean list that your AWeber form on your website is bound to.<br/>
+    You can set this setting in <b>List Options > List Settings > Confirmation Message > Confirmation Success Page</b>.<br/><br/>
+    Redirection to your website is important because <b>that's where this plugin does it's job</b>, on your website.</br>
+    I recommend setting up a separate page for that. It doesn't have to be nothing fancy, but just being a <b>separate</b> page.
+  </p>
 </div>
