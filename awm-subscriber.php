a84ac01d3ba605245ad5a5e80e7f056e6c24171a
arkadyt <contact.me@arkadyt.dev>
2019-03-09 18:23:55 -0800

    Include licenses in every source file

--- awm-subscriber.php
+++ awm-subscriber.php
@@ -7,10 +7,28 @@
  * Plugin Name: AWM Subscriber
  * Description: Plugin subscribes a visitor to multiple AWeber lists once he subscribes to a AWeber list of your choice and confirms the subscription. Learn more on the Settings page.
  * Version:     0.9.1-alpha
- * Author:      Arkady Titenko
+ * Author:      Andrew Titenko
  * Author URI:  http://arkadyt.com
  * License:     GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * AWM Subscriber
+ * Copyright (C) 2020 Andrew Titenko
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html.
  */
 
 defined('ABSPATH') or die('Access denied.');
